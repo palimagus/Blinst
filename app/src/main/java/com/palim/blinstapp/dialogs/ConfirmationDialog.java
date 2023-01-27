f49eff8b884db46816e7c5805b13fc9e92df7fc9
Anorak <palimagus@outlook.fr>
2023-01-27 23:43:51 +0100

    Update confirmation dialog

--- app/src/main/java/com/palim/blinstapp/dialogs/ConfirmationDialog.java
+++ app/src/main/java/com/palim/blinstapp/dialogs/ConfirmationDialog.java
@@ -17,12 +17,25 @@ public class ConfirmationDialog extends DialogFragment {
         this.message = msg;
     }
 
+    OnConfirm listener;
+
+    public void setConfirmListener(OnConfirm listener) {
+        this.listener = listener;
+    }
+
+    public interface OnConfirm {
+        void confirm();
+    }
+
     @Override
     @NonNull
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setMessage(this.message)
                 .setPositiveButton("Ok", ((dialogInterface, i) -> {
+                    if (this.listener != null) {
+                        this.listener.confirm();
+                    }
                     Objects.requireNonNull(ConfirmationDialog.this.getDialog()).cancel();
                 }));
         return builder.create();
